c1829ef527a4fbf435bf2e2a3ec3256149c9a2fa
DESKTOP-TUVHL3G\Lokiji <igor333m@yahoo.com>
2017-12-29 19:28:36 -0800

    feat: MVC updates

--- js/app.js
+++ js/app.js
@@ -36,14 +36,40 @@ $(function(){
 	let octopus = {
 		init: function() {
 			viewList.init();
+			viewCat.init();
+			octopus.catClick();
+			octopus.catClickCounter();
 		},
 
 		allCats: function() {
 			let cat = model.map( (cat, index, model) => {
 				return cat;
 			});
-			console.log(cat);
 			return cat;
+		},
+
+		currentCat: 0,
+
+		catClick: function() {
+			for(let i = 0; i < model.length; i++) {
+				$("li").click(function() {
+					console.log($(this).text());
+					if( $(this).text() === model[i].name ) {
+						// Set currentCat
+						octopus.currentCat = i;
+						// return clicked cat object
+						return viewCat.showCat(model[i]);
+					}
+				});
+			}
+		},
+
+		catClickCounter: function() {
+			$(".container").on("click", "img", function() {
+				console.log(octopus.currentCat);
+				model[octopus.currentCat].numClicks += 1;
+				viewCat.catClicksHTML(model[octopus.currentCat].numClicks);
+			});
 		}
 	};
 
@@ -60,7 +86,34 @@ $(function(){
 
 	//
 	let viewCat = {
+		// Create cat window
+		init: function() {
+			containerHtml = `
+				<article>
+					<h1>${model[0].name}</h1>
+					<img src="img/${model[0].img}" alt="${model[0].name} the cat" data-cat="${model[0].name}">
+		        	<h2>Number of clicks <span>${model[0].numClicks}</span></h2>
+		        </article>
+			`;
+			document.querySelector(".container").insertAdjacentHTML("afterbegin", containerHtml);
+		},
 
+		showCat: function(cat) {
+			// Clear .container
+			document.querySelector(".container").innerHTML = "";
+			containerHtml = `
+				<article>
+					<h1>${cat.name}</h1>
+					<img src="img/${cat.img}" alt="${cat.name} the cat" data-cat="${cat.name}">
+		        	<h2>Number of clicks <span>${cat.numClicks}</span></h2>
+		        </article>
+			`;
+			document.querySelector(".container").insertAdjacentHTML("afterbegin", containerHtml);
+		},
+
+		catClicksHTML: function(clicks) {
+			document.querySelector("h2 span").textContent = clicks;
+		}
 	}; 
 
 	octopus.init();
